bb1d54c84d71633181b2c2500e9a076e2e4a92b4
Naveen Kumar <naveenkumarkilli1509@gmail.com>
2025-07-22 14:48:49 +0530

    Fix: add dummy face_recognition module for deployment

---
+++ backend/face_recognition.py
@@ -0,0 +1,8 @@
+def load_image_file(file):
+    return None
+
+def face_encodings(image, known_face_locations=None, num_jitters=1, model="small"):
+    return []
+
+def compare_faces(known_face_encodings, face_encoding_to_check, tolerance=0.6):
+    return [False]
