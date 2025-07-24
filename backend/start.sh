03bfa2390bd3edaa83a533782090cb962516246a
Naveen Kumar <naveenkumarkilli1509@gmail.com>
2025-07-24 22:18:52 +0530

    Fix: deploy with Python 3.10 and clean pip install

--- backend/start.sh
+++ backend/start.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# Upgrade pip
 pip install --upgrade pip
-
-# Install base dependencies
 pip install -r requirements.txt
 
-# Install dlib and face-recognition using prebuilt wheel URLs
-pip install https://github.com/ageitgey/face_recognition/releases/download/v1.3.0/face_recognition-1.3.0-py2.py3-none-any.whl
-pip install https://github.com/RPi-Distro/dlib/releases/download/v19.22.99/dlib-19.22.99-cp310-cp310-manylinux2014_x86_64.whl
+# Install face-recognition using normal pip (which includes dlib)
+pip install face-recognition==1.3.0
 
-# Start the app
 python app.py
